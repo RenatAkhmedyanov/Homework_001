5393c89d400f62507762dc0318fd4400b657e841
Renat <renatakhmedyanov@gmail.com>
2022-10-15 13:49:31 +0400

    Add Task 2

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Программа для нахождения наибольшего из двух целых чисел");
+
+Console.Write("Введите первое целое число: ");
+int inputOne = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе целое число: ");
+int inputTwo = int.Parse(Console.ReadLine()!);
+
+if(inputOne > inputTwo){
+    Console.WriteLine("Наибольшее число " + inputOne);
+    Console.WriteLine("Наименьшее число " + inputTwo);
+} else {
+    Console.WriteLine("Наибольшее число " + inputTwo);
+    Console.WriteLine("Наименьшее число " + inputOne);
+}
