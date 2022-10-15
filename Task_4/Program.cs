631fe0f4faaa861c051f4e3e540b59e7e3e9f7b0
Renat <renatakhmedyanov@gmail.com>
2022-10-15 14:02:55 +0400

    Add Task 4

---
+++ Task_4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Программа для нахождения максимального из трех целых чисел.");
+
+Console.Write("Введите первое число: ");
+int inputOne = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int inputTwo = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье число: ");
+int inputThree = int.Parse(Console.ReadLine()!);
+
+int max = inputOne;
+if (inputTwo > max) max = inputTwo;
+if (inputThree > max) max = inputThree;
+
+Console.WriteLine("Максимальное из введеных чисел " + max);
