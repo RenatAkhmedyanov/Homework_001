aa1991323bc13dad4f7cc8081e1b28e2e29456de
Renat <renatakhmedyanov@gmail.com>
2022-10-15 14:10:51 +0400

    Add Task 6

---
+++ Task_6/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Программа для проверки четности целого числа.");
+
+Console.Write("Введите целое число: ");
+int input = int.Parse(Console.ReadLine()!);
+
+if (input % 2 == 0){
+    Console.WriteLine("Введенное число четное");
+} else {
+    Console.WriteLine("Введенное число нечетное");
+}
