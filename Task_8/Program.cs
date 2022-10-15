080dc9bd343dcc6cbb04bc1e6c42aa96a8b98e6c
Renat <renatakhmedyanov@gmail.com>
2022-10-15 14:32:06 +0400

    Add Task 8

---
+++ Task_8/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Программа для вывода всех четных чисел в промежутке от 1 до N.");
+
+Console.Write("Введите положительное значение для числа N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+if (N <= 0) Console.WriteLine("Ошибка ввода. Введите положительное число!");
+
+if (N == 1) Console.WriteLine("Четных чисел нет.");
+
+if (N > 1){
+    int count = 1;
+    while (count <= N){
+        if (count % 2 == 0) Console.Write(count + " ");
+        count++;
+    }
+        
+}
